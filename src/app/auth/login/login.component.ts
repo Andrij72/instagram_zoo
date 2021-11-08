f6edec4bd01f67fa34bac096838001ffdec98bc8
Andrij72 <akulmmm@gmail.com>
2021-11-08 12:14:45 +0200

    Add client module and some fix server module

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from "@angular/forms";
+import {AuthService} from "../../service/auth.service";
+import {TokenStorageService} from "../../service/token-storage.service";
+import {Router} from "@angular/router";
 import {FormGroup} from "@angular/forms";
 
 @Component({
@@ -8,8 +12,19 @@ import {FormGroup} from "@angular/forms";
 })
 export class LoginComponent implements OnInit {
 
- // public loginForm: FormGroup;
-  constructor() { }
+  public loginForm: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private tokenStorage: TokenStorageService,
+    private notificationService: Notification,
+    private router: Router,
+    private fb: FormBuilder) {
+    if(this.tokenStorage.getUser()) {
+
+    }
+  }
+
 
   ngOnInit(): void {
   }
--- src/app/service/token-storage.service.ts
+++ src/app/service/token-storage.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {User} from "../models/User";
+
+const TOKEN_KEY = 'auth-token';
+const USER_TOKEN = 'auth-user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenStorageService {
 
-  constructor() { }
+  constructor() {
+  }
+
+  public saveToken(token: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+  }
+
+  public getToken(): string {
+    return <string>sessionStorage.getItem(TOKEN_KEY);
+  }
+
+  public saveUser(user: User): void {
+    window.sessionStorage.removeItem(USER_TOKEN);
+    window.sessionStorage.setItem(USER_TOKEN, JSON.stringify(user));
+  }
+
+  public getUser(): any {
+    return JSON.parse(<string>sessionStorage.getItem(USER_TOKEN));
+  }
+
+  logOut(): void {
+    window.sessionStorage.clear();
+    window.location.reload();
+  }
+
 }
